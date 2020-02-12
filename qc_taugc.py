4c6e9415df2ce9b6daeb2cc206953d68259b961a
gebarros <gebarrosbio@gmail.com>
2020-02-12 17:28:00 -0300

    first page pdf

--- qc_taugc.py
+++ qc_taugc.py
@@ -35,11 +35,14 @@ parser.add_argument("-r",
 
 args = parser.parse_args()
 
-qc_rnaseq = qc_helper.QualityControlRNAseq()
+qc_rnaseq = qc_helper.QualityControl()
 experiment = args.e[0]
 type_seq = args.t[0]
 sample = args.id[0]
 out_dir = '{}_out_dir'.format(sample)
+adapters = os.path.abspath('src/adapters.fa')
+
+print(adapters)
 
 if not os.path.exists(out_dir):
     out_dir = '{}_out_dir'.format(sample)
@@ -53,19 +56,21 @@ if experiment == 0:
     r1=args.f[0]
     r2=args.r[0]
     os.system('trimmomatic PE -phred33 {} {} {}/{}_L001_R1_001_paired.fq.gz {}/{}_L001_R1_001_unpaired.fq.gz {}/{}_L001_R2_001_paired.fq.gz {}/{}_L001_R2_001_unpaired.fq.gz \
-               ILLUMINACLIP:/src/adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
-               .format(r1,r2,out_dir,sample,out_dir,sample,out_dir,sample,out_dir,sample))
+               ILLUMINACLIP:{}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
+               .format(r1,r2,out_dir,sample,out_dir,sample,out_dir,sample,out_dir,sample,adapters))
+    
+    os.system('fastqc {}/*_paired*' .format(out_dir))
    
   elif type_seq == 'SE' or type_seq == 'se':
     r1=args.f[0]  
     os.system('trimmomatic SE -phred33 {} {}/{}_L001_R1_001_paired.fq.gz \
-               ILLUMINACLIP:/src/adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
-               .format(r1,out_dir,sample))
+               ILLUMINACLIP:{}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
+               .format(r1,out_dir,sample,adapters))
+
+    os.system('fastqc {}/*_paired*' .format(out_dir))
     
   else:
     print('Error: Invalid parameter!')
-  
-  os.system('fastqc {}/*_paired*' .format(out_dir))
 
 else:
   print('Sorry, there is just RNAseq module for while!')
--- src/qc_helper.py
+++ src/qc_helper.py
@@ -1,12 +1,48 @@
+# -*- coding: utf-8 -*-
+from datetime import date
 import os
+from reportlab.pdfgen import canvas
 
-class QualityControlRNAseq:
+def firstPage(doc):
+  logo = os.path.abspath('images/taugc.png')
+  doc.drawImage(logo, 200, 450)
 
-  pass
+  doc.setStrokeColorRGB(0,0,0.5)
+  doc.setFillColorRGB(0,0,0.5)
+  doc.rect(0,300, 290,40, fill=1)
+
+  doc.setStrokeColorRGB(0,0.5,0)
+  doc.setFillColorRGB(0,0.5,0)
+  doc.rect(310,300, 285 ,40, fill=1)
+
+  doc.setFillColorRGB(255,255,255)
+  doc.setFont('Helvetica-Bold', 25)
+  doc.drawString(12,310, "Quality Control Report")
+  
+  today = date.today()
+  x = today.strftime('%B, %d %Y')
+  doc.setFillColorRGB(255,255,255)
+  doc.setFont('Helvetica-Bold', 25)
+  doc.drawString(350,310, x)
+
+  doc.setFillColorRGB(0,0,0)
+  doc.setFont('Helvetica-Bold', 20)
+  doc.drawString(265,200, "Contact:")
+
+  doc.setFillColorRGB(0,0,0)
+  doc.setFont('Helvetica', 20)
+  doc.drawString(350,200, "info@taugc.com")
+ 
+  
 
 
 
+sample = 'TESTE'
+doc = canvas.Canvas('QCReport_{}.pdf'.format(sample))   
 
+firstPage(doc)
+doc.showPage()
+doc.save()
 
 
 
