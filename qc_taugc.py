18fecb759f8df22904dc164ed8bf83e657f535d0
gebarros <gebarrosbio@gmail.com>
2020-02-13 16:20:31 -0300

    first pages ready

--- qc_taugc.py
+++ qc_taugc.py
@@ -3,8 +3,19 @@
 import argparse
 import os
 import sys
+from datetime import date
 from src import qc_helper
-from reportlab.pdfgen import canvas
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.graphics.shapes import *
+from reportlab.graphics import renderPDF
+from reportlab.graphics.charts.barcharts import VerticalBarChart
+from reportlab.platypus import Image, Paragraph, BaseDocTemplate, Frame, PageTemplate, SimpleDocTemplate, Spacer, PageBreak, Flowable, NextPageTemplate
+from reportlab.lib.pagesizes import A4,landscape, letter
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.units import inch, cm, mm
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib import colors
+from reportlab.lib.enums import TA_JUSTIFY, TA_LEFT, TA_CENTER, TA_RIGHT
 
 parser = argparse.ArgumentParser(description='QC TauGC: used to make the quality control from NGS fastq files and generate an automatic report.')
 
@@ -36,10 +47,11 @@ parser.add_argument("-r",
 
 args = parser.parse_args()
 
-qc_report = qc_helper.QCReport()
+pr = qc_helper.ProcessResult()
 experiment = args.e[0]
 type_seq = args.t[0]
 sample = args.id[0]
+r1 = args.f[0]
 out_dir = '{}_out_dir'.format(sample)
 adapters = os.path.abspath('src/adapters.fa')
 
@@ -47,24 +59,23 @@ if not os.path.exists(out_dir):
     out_dir = '{}_out_dir'.format(sample)
     os.makedirs(out_dir)
 
+n_reads = pr.get_n_reads(r1)
+sequencing = pr.get_typeseq(type_seq)
+
+##########################################################################
 ############################ RNAseq ######################################
+##########################################################################
 if experiment == 0:
   if type_seq == 'PE' or type_seq == 'pe':
     r1=args.f[0]
     r2=args.r[0]
-    #os.system('trimmomatic PE -phred33 {} {} {}/{}_L001_R1_001_paired.fq.gz {}/{}_L001_R1_001_unpaired.fq.gz {}/{}_L001_R2_001_paired.fq.gz {}/{}_L001_R2_001_unpaired.fq.gz \
-    #           ILLUMINACLIP:{}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
-    #           .format(r1,r2,out_dir,sample,out_dir,sample,out_dir,sample,out_dir,sample,adapters))
-
-    #os.system('fastqc {}/*_paired*' .format(out_dir))
+    pr.run_trimPE(r1,r2,out_dir,sample,adapters)
+    pr.run_fastQC(out_dir)
 
   elif type_seq == 'SE' or type_seq == 'se':
     r1=args.f[0]
-    #os.system('trimmomatic SE -phred33 {} {}/{}_L001_R1_001_paired.fq.gz \
-    #           ILLUMINACLIP:{}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
-    #           .format(r1,out_dir,sample,adapters))
-
-    #os.system('fastqc {}/*_paired*' .format(out_dir))
+    pr.run_trimSE(r1,out_dir,sample,adapters)
+    pr.run_fastQC(out_dir)
 
   else:
     print('Error: Invalid parameter!')
@@ -72,18 +83,118 @@ if experiment == 0:
 else:
   print('Sorry, there is just RNAseq module for while!')
 
-doc = canvas.Canvas('QCReport_{}.pdf'.format(sample))
-qc_report.firstPage(doc)
-doc.showPage()
-doc.save()
 
 
 
 
 
 
+##########################################################################
+############################ REPORT ######################################
+##########################################################################
+doc = SimpleDocTemplate('QCReport_{}.pdf'.format(sample))
+styles = getSampleStyleSheet()
+styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+styles.add(ParagraphStyle(name='Center', alignment=TA_CENTER)) 
+styles.add(ParagraphStyle(name='Left', alignment=TA_LEFT)) 
+styles.add(ParagraphStyle(name='Right', alignment=TA_RIGHT))
+styles.add(ParagraphStyle(name='Infos', alignment=TA_RIGHT, fontName='Helvetica-Bold'))
+styles.add(ParagraphStyle(name='Disclaimer', alignment=TA_CENTER, fontName='Helvetica-Bold')) 
+styles.add(ParagraphStyle(name='DisclaimerText', alignment=TA_JUSTIFY, fontName='Helvetica', firstLineIndent=40))
+
+spacer = Spacer(0, 0.7*inch)
+Story = []
+######################################
+#################### First page
+######################################
+# logo
+logo = os.path.abspath('images/taugc.png')
+im = Image(logo, 3*inch,3*inch)
+Story.append(im)
+Story.append(spacer)
+
+# rect blue
+d = Drawing(500, 200)
+d.add(Rect(-75, 120, 280, 40, fillColor=colors.darkblue))
+d.add(String(-68,132, 'Quality Control Report', fontSize=25, fontName='Helvetica-Bold', fillColor=colors.white))
+Story.append(d)
+
+# rect blue
+today = date.today()
+x = today.strftime('%B, %d %Y')
+e = Drawing(500, 200)
+e.add(Rect(233, 320, 280, 40, fillColor=colors.darkgreen))
+e.add(String(260,330, x, fontSize=25, fontName='Helvetica-Bold', fillColor=colors.white))
+Story.append(e)
+
+# Contact
+Story.append(Spacer(0, -3*inch))
+email = '<font size=12>%s</font>' % "Contact us: info@taugc.com"
+Story.append(Paragraph(email, styles["Infos"]))
+Story.append(Spacer(0, 0.2*inch))
+site = '<font size=12>%s</font>' % "About us: www.taugc.com"
+Story.append(Paragraph(site, styles["Infos"]))
+Story.append(Spacer(0, 1*inch))
+
+#Disclaimer first page
+disclaimer_title = '<font size=13 textColor="black">%s</font>' % "DISCLAIMER"
+Story.append(Paragraph(disclaimer_title, styles["Disclaimer"]))
+Story.append(Spacer(0, 0.2*inch))
+Story.append(Spacer(5, 0))
+disclaimer_text = '<font size=10 textColor="grey">%s</font>' % "  This report is intended for general guidance and information purposes only. This report is under no circumstances intended to be used or considered directly for any kind of scientific or academic publication and clinical diagnosis.  Please note that this is reference material for the original authors of the study. The report is to be considered only as a tool for hypotheses and data interpretation by the authors along with all data provided."
+Story.append(Paragraph(disclaimer_text, styles["DisclaimerText"]))
+Story.append(PageBreak()) 
+######################################
+#################### Second page
+######################################
+logo = os.path.abspath('images/taugc.png')
+im = Image(logo, 0.8*inch,0.8*inch)
+im.hAlign = 'LEFT'
+im.vAlign = 'TOP'
+Story.append(im)
+
+disclaimer_title = '<font size=13 textColor="black">%s</font>' % "DISCLAIMER"
+Story.append(Paragraph(disclaimer_title, styles["Disclaimer"]))
+Story.append(Spacer(0, 0.2*inch))
+disclaimer_2 = '<font size=10 textColor="grey">%s</font>' % "The data used for constructing this material in the report is obtained directly from the customer (customer). We have taken reasonable care to ensure that, and to the best of our knowledge, material information contained herein is in accordance with information and guidelines provided by the customer. All data presented are prepared by TauGC Bioinformatics in accordance with best practices from third parties, as part of the service included in the report. The estimates are subject to uncertainties, misinterpretations and other factors that may cause actual events to differ materially from any anticipated hypotheses. Please note that we make no assurance that the underlying statements are free from errors. Neither the authors nor TauGC Bioinformatics is making any representation or warranty, express or implied, as to the accuracy or completeness of data presented in this report and none of the authors or TauGC Bioinformatics will have any liability towards any other person resulting in incorrect use this report. TauGC do not intend, and do not assume any obligation to update or correct the information included in this report." 	
+Story.append(Paragraph(disclaimer_2, styles["DisclaimerText"]))
+Story.append(Spacer(0, 0.1*inch))
+disclaimer_3 = '<font size=10 textColor="grey">%s</font>' % "The information contained herein may be subject to interpretation by authors. TauGC does not accept any form of liability, neither legally nor financially, for loss (direct or indirect) caused by the understanding and/or use of this report or its content." 	
+Story.append(Paragraph(disclaimer_3, styles["DisclaimerText"]))
+Story.append(Spacer(0, 0.1*inch))
+disclaimer_4 = '<font size=10 textColor="grey">%s</font>' % "This report is only intended for the recipients, and should not be copied or otherwise distributed, in whole or in part, to any other person. This report is subject to Brazilian law, and any dispute arising in respect of this report is subject to the exclusive jurisdiction of Brazilian courts." 	
+Story.append(Paragraph(disclaimer_4, styles["DisclaimerText"]))
+
+Story.append(Spacer(0, 0.3*inch))
+disclaimer_5 = '<font size=12 textColor="grey">%s</font>' % "CONSIDERATIONS ABOUT AUTHORSHIP & LIABILITY"
+Story.append(Paragraph(disclaimer_5, styles["Disclaimer"]))
+Story.append(Spacer(0, 0.2*inch))
+disclaimer_6 = '<font size=10 textColor="grey">%s</font>' % "TauGC Bioinformatics is not to be considered as author or co-author of any kind publication solely based on direct use of this report. Any consideration for authorship by TauGC must be discussed further by authors and TauGC Bioinformatics." 	
+Story.append(Paragraph(disclaimer_6, styles["DisclaimerText"]))
+Story.append(Spacer(0, 0.1*inch))
+disclaimer_7 = '<font size=10 textColor="grey">%s</font>' % "TauGC and its members follow the Nature Journals Policy for Authorship (https://www.nature.com/authors/policies/authorship.html) adapted from McNutt et al. 2018 (DOI: 10.1073/pnas.1715374115)." 	
+Story.append(Paragraph(disclaimer_7, styles["DisclaimerText"]))
+
+# footer
+Story.append(Spacer(0, 2.7*inch))
+footer_1 = '<font size=8 textColor="grey">%s</font>' % "TauGC Bioinformatics - Rua Apiacas 886, São Paulo - SP" 	
+Story.append(Paragraph(footer_1, styles["Center"]))
+Story.append(Spacer(0, 0.1*inch))
+footer_2 = '<font size=8 textColor="grey">%s</font>' % "This report is only intended for the recipients, and should not be copied or otherwise distributed, in whole or in part, to any other person." 	
+Story.append(Paragraph(footer_2, styles["Center"]))
+Story.append(PageBreak())
+
+######################################
+#################### Third page
+######################################
+logo = os.path.abspath('images/taugc.png')
+im = Image(logo, 0.8*inch,0.8*inch)
+im.hAlign = 'LEFT'
+im.vAlign = 'TOP'
+Story.append(im)
 
 
 
 
 
+doc.build(Story)
--- src/qc_helper.py
+++ src/qc_helper.py
@@ -1,58 +1,35 @@
 # -*- coding: utf-8 -*-
-from datetime import date
 import os
-from reportlab.pdfgen import canvas
-from textwrap import wrap
 
+class ProcessResult:
 
-class QCReport:
+  def run_trimPE(self,r1,r2,out_dir,sample,adapters):
+    os.system('trimmomatic PE -phred33 {} {} {}/{}_L001_R1_001_paired.fq.gz {}/{}_L001_R1_001_unpaired.fq.gz {}/{}_L001_R2_001_paired.fq.gz {}/{}_L001_R2_001_unpaired.fq.gz \
+              ILLUMINACLIP:{}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
+              .format(r1,r2,out_dir,sample,out_dir,sample,out_dir,sample,out_dir,sample,adapters))
 
-  def firstPage(canvas,doc):
-    logo = os.path.abspath('images/taugc.png')
-    doc.drawImage(logo, 200, 500)
+  def run_trimSE(self, r1,out_dir,sample,adapters):
+    os.system('trimmomatic SE -phred33 {} {}/{}_L001_R1_001_paired.fq.gz \
+              ILLUMINACLIP:{}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
+              .format(r1,out_dir,sample,adapters))
 
-    doc.setStrokeColorRGB(0,0,0.5)
-    doc.setFillColorRGB(0,0,0.5)
-    doc.rect(0,400, 290,40, fill=1)
+  def run_fastQC(self, out_dir):
+    os.system('fastqc {}/*_paired*' .format(out_dir))
 
-    doc.setStrokeColorRGB(0,0.5,0)
-    doc.setFillColorRGB(0,0.5,0)
-    doc.rect(310,400, 285 ,40, fill=1)
+  def get_typeseq(self, typeseq):
+    if typeseq == 'PE' or typeseq == 'pe':
+      return 'Pair-End'
+    elif typeseq == 'SE' or typeseq == 'se':
+      return 'Single-End'
+    else:
+      return 'Unknow'
 
-    doc.setFillColorRGB(255,255,255)
-    doc.setFont('Helvetica-Bold', 25)
-    doc.drawString(12,410, "Quality Control Report")
+  def get_n_reads(self, path):
+    lines = os.system('zcat {} | wc -l'.format(path))
+    total_reads = int(lines) / 4
+    return total_reads
 
-    today = date.today()
-    x = today.strftime('%B, %d %Y')
-    doc.setFillColorRGB(255,255,255)
-    doc.setFont('Helvetica-Bold', 25)
-    doc.drawString(350,410, x)
-
-    doc.setFillColorRGB(0,0,0)
-    doc.setFont('Helvetica-Bold', 20)
-    doc.drawString(265,270, "Contact:")
-
-    doc.setFillColorRGB(0,0,0)
-    doc.setFont('Helvetica', 20)
-    doc.drawString(350,270, "info@taugc.com")
-
-    doc.setFillColorRGB(0.5,0.5,0.5)
-    doc.setFont('Helvetica-Bold', 14)
-    doc.drawString(255,180, "Disclaimer")
-
-    t = doc.beginText()
-    t.setFont('Helvetica', 10)
-    t.setFillColorRGB(0.5,0.5,0.5)
-    t.setCharSpace(2)
-    t.setTextOrigin(30, 150)
-    text = "This report is intended for general guidance and information purposes only. This report is under no circumstances intended to be used or considered directly for any kind of scientific or academic publication and clinical diagnosis. Please note that this is reference material for the original authors of the study. The report is to be considered only as a tool for hypotheses and data interpretation by the authors along with all data provided."
-    wraped_text = "\n".join(wrap(text, 85)) # 85 is line width
-    t.textLines(wraped_text)
-    doc.drawText(t)
-
-
-
+  
 
 
 
