c9f9c08fc1b491f018f04c14b77af3ed819bcfe3
gebarros <gebarrosbio@gmail.com>
2020-02-13 00:37:10 -0300

    disclaimer

--- qc_taugc.py
+++ qc_taugc.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import sys
 from src import qc_helper
+from reportlab.pdfgen import canvas
 
 parser = argparse.ArgumentParser(description='QC TauGC: used to make the quality control from NGS fastq files and generate an automatic report.')
 
@@ -35,45 +36,46 @@ parser.add_argument("-r",
 
 args = parser.parse_args()
 
-qc_rnaseq = qc_helper.QualityControl()
+qc_report = qc_helper.QCReport()
 experiment = args.e[0]
 type_seq = args.t[0]
 sample = args.id[0]
 out_dir = '{}_out_dir'.format(sample)
 adapters = os.path.abspath('src/adapters.fa')
 
-print(adapters)
-
 if not os.path.exists(out_dir):
     out_dir = '{}_out_dir'.format(sample)
     os.makedirs(out_dir)
-
-print(experiment,type_seq,sample,out_dir)
 
 ############################ RNAseq ######################################
 if experiment == 0:
   if type_seq == 'PE' or type_seq == 'pe':
     r1=args.f[0]
     r2=args.r[0]
-    os.system('trimmomatic PE -phred33 {} {} {}/{}_L001_R1_001_paired.fq.gz {}/{}_L001_R1_001_unpaired.fq.gz {}/{}_L001_R2_001_paired.fq.gz {}/{}_L001_R2_001_unpaired.fq.gz \
-               ILLUMINACLIP:{}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
-               .format(r1,r2,out_dir,sample,out_dir,sample,out_dir,sample,out_dir,sample,adapters))
-    
-    os.system('fastqc {}/*_paired*' .format(out_dir))
-   
-  elif type_seq == 'SE' or type_seq == 'se':
-    r1=args.f[0]  
-    os.system('trimmomatic SE -phred33 {} {}/{}_L001_R1_001_paired.fq.gz \
-               ILLUMINACLIP:{}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
-               .format(r1,out_dir,sample,adapters))
+    #os.system('trimmomatic PE -phred33 {} {} {}/{}_L001_R1_001_paired.fq.gz {}/{}_L001_R1_001_unpaired.fq.gz {}/{}_L001_R2_001_paired.fq.gz {}/{}_L001_R2_001_unpaired.fq.gz \
+    #           ILLUMINACLIP:{}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
+    #           .format(r1,r2,out_dir,sample,out_dir,sample,out_dir,sample,out_dir,sample,adapters))
 
-    os.system('fastqc {}/*_paired*' .format(out_dir))
-    
+    #os.system('fastqc {}/*_paired*' .format(out_dir))
+
+  elif type_seq == 'SE' or type_seq == 'se':
+    r1=args.f[0]
+    #os.system('trimmomatic SE -phred33 {} {}/{}_L001_R1_001_paired.fq.gz \
+    #           ILLUMINACLIP:{}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:35'
+    #           .format(r1,out_dir,sample,adapters))
+
+    #os.system('fastqc {}/*_paired*' .format(out_dir))
+
   else:
     print('Error: Invalid parameter!')
 
 else:
   print('Sorry, there is just RNAseq module for while!')
+
+doc = canvas.Canvas('QCReport_{}.pdf'.format(sample))
+qc_report.firstPage(doc)
+doc.showPage()
+doc.save()
 
 
 
--- src/qc_helper.py
+++ src/qc_helper.py
@@ -2,47 +2,63 @@
 from datetime import date
 import os
 from reportlab.pdfgen import canvas
-
-def firstPage(doc):
-  logo = os.path.abspath('images/taugc.png')
-  doc.drawImage(logo, 200, 450)
-
-  doc.setStrokeColorRGB(0,0,0.5)
-  doc.setFillColorRGB(0,0,0.5)
-  doc.rect(0,300, 290,40, fill=1)
-
-  doc.setStrokeColorRGB(0,0.5,0)
-  doc.setFillColorRGB(0,0.5,0)
-  doc.rect(310,300, 285 ,40, fill=1)
-
-  doc.setFillColorRGB(255,255,255)
-  doc.setFont('Helvetica-Bold', 25)
-  doc.drawString(12,310, "Quality Control Report")
-  
-  today = date.today()
-  x = today.strftime('%B, %d %Y')
-  doc.setFillColorRGB(255,255,255)
-  doc.setFont('Helvetica-Bold', 25)
-  doc.drawString(350,310, x)
-
-  doc.setFillColorRGB(0,0,0)
-  doc.setFont('Helvetica-Bold', 20)
-  doc.drawString(265,200, "Contact:")
-
-  doc.setFillColorRGB(0,0,0)
-  doc.setFont('Helvetica', 20)
-  doc.drawString(350,200, "info@taugc.com")
- 
-  
+from textwrap import wrap
 
 
+class QCReport:
 
-sample = 'TESTE'
-doc = canvas.Canvas('QCReport_{}.pdf'.format(sample))   
+  def firstPage(canvas,doc):
+    logo = os.path.abspath('images/taugc.png')
+    doc.drawImage(logo, 200, 500)
 
-firstPage(doc)
-doc.showPage()
-doc.save()
+    doc.setStrokeColorRGB(0,0,0.5)
+    doc.setFillColorRGB(0,0,0.5)
+    doc.rect(0,400, 290,40, fill=1)
+
+    doc.setStrokeColorRGB(0,0.5,0)
+    doc.setFillColorRGB(0,0.5,0)
+    doc.rect(310,400, 285 ,40, fill=1)
+
+    doc.setFillColorRGB(255,255,255)
+    doc.setFont('Helvetica-Bold', 25)
+    doc.drawString(12,410, "Quality Control Report")
+
+    today = date.today()
+    x = today.strftime('%B, %d %Y')
+    doc.setFillColorRGB(255,255,255)
+    doc.setFont('Helvetica-Bold', 25)
+    doc.drawString(350,410, x)
+
+    doc.setFillColorRGB(0,0,0)
+    doc.setFont('Helvetica-Bold', 20)
+    doc.drawString(265,270, "Contact:")
+
+    doc.setFillColorRGB(0,0,0)
+    doc.setFont('Helvetica', 20)
+    doc.drawString(350,270, "info@taugc.com")
+
+    doc.setFillColorRGB(0.5,0.5,0.5)
+    doc.setFont('Helvetica-Bold', 14)
+    doc.drawString(255,180, "Disclaimer")
+
+    t = doc.beginText()
+    t.setFont('Helvetica', 10)
+    t.setFillColorRGB(0.5,0.5,0.5)
+    t.setCharSpace(2)
+    t.setTextOrigin(30, 150)
+    text = "This report is intended for general guidance and information purposes only. This report is under no circumstances intended to be used or considered directly for any kind of scientific or academic publication and clinical diagnosis. Please note that this is reference material for the original authors of the study. The report is to be considered only as a tool for hypotheses and data interpretation by the authors along with all data provided."
+    wraped_text = "\n".join(wrap(text, 85)) # 85 is line width
+    t.textLines(wraped_text)
+    doc.drawText(t)
+
+
+
+
+
+
+
+
+
 
 
 
